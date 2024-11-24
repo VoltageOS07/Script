f1aae148c765ca04ecbf94affc83b2764a303142
Sachinpawar86 <106669438+Sachinpawar86@users.noreply.github.com>
2024-11-24 11:01:42 +0530

    Update voltage.sh

--- voltage.sh
+++ voltage.sh
@@ -17,7 +17,6 @@ git clone https://gitlab.com/sachinbarange86/android_vendor_xiaomi_mojito.git --
 git clone https://gitlab.com/sachinbarange86/android_vendor_xiaomi_sm6150-common.git --depth 1 -b 14 vendor/xiaomi/sm6150-common
 # hardware tree
 git clone https://github.com/VoltageOS07/android_hardware_xiaomi.git --depth 1 -b mojito hardware/xiaomi
-git clone https://github.com/LineageOS/android_packages_resources_devicesettings.git --depth 1 -b lineage-21.0 packages/resources/devicesettings
 # set build environment
 . build/envsetup.sh
 # lunch
